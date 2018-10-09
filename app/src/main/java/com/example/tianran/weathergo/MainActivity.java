568992be160f3bf55cd6b70fb565a8819a218c92
Tianran <tianran_oy@pku.edu.cn>
2018-10-09 22:52:29 +0800

    weather04-05_finished

--- app/src/main/java/com/example/tianran/weathergo/MainActivity.java
+++ app/src/main/java/com/example/tianran/weathergo/MainActivity.java
@@ -1,12 +1,97 @@
 package com.example.tianran.weathergo;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity{
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import cn.edu.pku.tianran.util.NetUtil;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+    private ImageView mUpdateBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        mUpdateBtn = (ImageView) findViewById(R.id.title_update_btn);
+        mUpdateBtn.setOnClickListener(this);
+
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE){
+            Log.d("WeatherGo","网络ok");
+            Toast.makeText(this, "网络ok！", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Log.d("WeatherGo","网络挂了");
+            Toast.makeText(MainActivity.this,"网络挂了",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    /**
+     *
+     * @param cityCode
+     */
+    private void queryWeatherCode(String cityCode){
+        final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + cityCode;
+        Log.d("WeatherGo",address);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection con = null;
+                try{
+                    URL url = new URL(address);
+                    con = (HttpURLConnection)url.openConnection();
+                    con.setRequestMethod("GET");
+                    con.setConnectTimeout(8000);
+                    con.setReadTimeout(8000);
+                    InputStream in = con.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String str;
+                    while((str=reader.readLine()) != null){
+                        response.append(str);
+                        Log.d("WeatherGo", str);
+                    }
+                    String responseStr=response.toString();
+                    Log.d("WeatherGo", responseStr);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    if(con != null) {
+                        con.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view.getId() == R.id.title_update_btn){
+            SharedPreferences sharedPreferences = getSharedPreferences("config",MODE_PRIVATE);
+            String cityCode = sharedPreferences.getString("main_city_code","101010100");
+            Log.d("WeatherGo","cityCode");
+
+            if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE){
+                Log.d("WeatherGo","网络ok");
+                queryWeatherCode(cityCode);
+            }else{
+                Log.d("WeatherGo","网络挂了");
+                Toast.makeText(MainActivity.this,"网络挂了！",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
