fdd3f57226aef118f6a3dd625656b8d462155aad
Tianran <tianran_oy@pku.edu.cn>
2018-10-15 10:07:01 +0800

    update_weather_info_unfinished

--- app/src/main/java/com/example/tianran/weathergo/MainActivity.java
+++ app/src/main/java/com/example/tianran/weathergo/MainActivity.java
@@ -7,11 +7,18 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -20,6 +27,9 @@ import cn.edu.pku.tianran.util.NetUtil;
 public class MainActivity extends Activity implements OnClickListener{
 
     private ImageView mUpdateBtn;
+    private TextView cityTv,timeTv,humidityTv,weekTv,pmDataTv,pmQualityTv,
+            temperatureTv,climateTv,windTv,city_name_Tv;
+    private ImageView weatherImg,pmImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -37,6 +47,33 @@ public class MainActivity extends Activity implements OnClickListener{
             Log.d("WeatherGo","网络挂了");
             Toast.makeText(MainActivity.this,"网络挂了",Toast.LENGTH_SHORT).show();
         }
+
+        initView();
+    }
+
+    void initView(){
+        city_name_Tv = (TextView) findViewById(R.id.title_city_name);
+        cityTv = (TextView) findViewById(R.id.city);
+        timeTv = (TextView) findViewById(R.id.time);
+        humidityTv = (TextView) findViewById(R.id.humidity);
+        weekTv = (TextView) findViewById(R.id.week_today);
+        pmDataTv = (TextView) findViewById(R.id.pm_data);
+        pmQualityTv = (TextView) findViewById(R.id.pm2_5_quality);
+        pmImg = (ImageView) findViewById(R.id.pm2_5_img);
+        temperatureTv = (TextView) findViewById(R.id.temperature);
+        climateTv = (TextView) findViewById(R.id.climate);
+        windTv = (TextView) findViewById(R.id.wind);
+        weatherImg = (ImageView) findViewById(R.id.weather_img);
+        city_name_Tv.setText("N/A");
+        cityTv.setText("N/A");
+        timeTv.setText("N/A");
+        humidityTv.setText("N/A");
+        pmDataTv.setText("N/A");
+        pmQualityTv.setText("N/A");
+        weekTv.setText("N/A");
+        temperatureTv.setText("N/A");
+        climateTv.setText("N/A");
+        windTv.setText("N/A");
     }
 
 
@@ -67,6 +104,7 @@ public class MainActivity extends Activity implements OnClickListener{
                     }
                     String responseStr=response.toString();
                     Log.d("WeatherGo", responseStr);
+                    parseXML(responseStr);
                 }catch (Exception e){
                     e.printStackTrace();
                 }finally {
@@ -93,5 +131,80 @@ public class MainActivity extends Activity implements OnClickListener{
                 Toast.makeText(MainActivity.this,"网络挂了！",Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    private void parseXML(String xmldata){
+        int fengxiangCount=0;
+        int fengliCount =0;
+        int dateCount = 0;
+        int highCount = 0;
+        int lowCount = 0;
+        int typeCount =0;
+
+        try{
+            XmlPullParserFactory fac = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = fac.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmldata));
+            int eventType = xmlPullParser.getEventType();
+            Log.d("WeatherGo","parseXML");
+            while (eventType != XmlPullParser.END_DOCUMENT){
+                switch (eventType) {
+                    case XmlPullParser.START_DOCUMENT:
+                        break;
+                    case XmlPullParser.START_TAG:
+                        if (xmlPullParser.getName().equals("city")){
+                            eventType = xmlPullParser.next();
+                            Log.d("WeatherGo","city:"+xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("updatetime")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("WeatherGo", "updatetime:"+xmlPullParser.getText());
+                        }else if (xmlPullParser.getName().equals("shidu")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("WeatherGo", "shidu:"+xmlPullParser.getText());
+                        }else if (xmlPullParser.getName().equals("wendu")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("WeatherGo", "wendu:"+xmlPullParser.getText());
+                        }else if (xmlPullParser.getName().equals("pm25")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("WeatherGo", "pm25:"+xmlPullParser.getText());
+                        }else if (xmlPullParser.getName().equals("quality")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("WeatherGo", "quality:"+xmlPullParser.getText());
+                        }else if (xmlPullParser.getName().equals("fengxiang") && fengxiangCount ==0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("WeatherGo", "fengxiang:"+xmlPullParser.getText());
+                            fengxiangCount++;
+                        }else if (xmlPullParser.getName().equals("fengli") && fengliCount ==0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("WeatherGo", "fengli:"+xmlPullParser.getText());
+                            fengliCount++;
+                        }else if (xmlPullParser.getName().equals("date") && dateCount ==0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("WeatherGo", "date:"+xmlPullParser.getText());
+                            dateCount++;
+                        }else if (xmlPullParser.getName().equals("high") && highCount ==0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("WeatherGo", "high:"+xmlPullParser.getText());
+                            highCount++;
+                        }else if (xmlPullParser.getName().equals("low") && lowCount ==0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("WeatherGo", "low:"+xmlPullParser.getText());
+                            lowCount++;
+                        }else if (xmlPullParser.getName().equals("type") && typeCount ==0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("WeatherGo", "type:"+xmlPullParser.getText());
+                            typeCount++;
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+            }catch (XmlPullParserException e){
+                e.printStackTrace();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
     }
 }
